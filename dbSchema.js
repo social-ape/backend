9ff7398b5b0c5a4bfb02032459f282ef7f9b56b5
Nitin verma <canvas.nv@gmail.com>
2019-07-17 17:39:31 +0530

    using express

---
+++ dbSchema.js
@@ -0,0 +1,11 @@
+let db={
+    screams:[
+        {
+            userHandle:'user',
+            body:'scream body',
+            createdAt:"2019-07-17T12:01:33.422Z",
+            likeCount:5,
+            commentCount:3
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const express = require("express");
+const app = express();
 
 var serviceAccount = require("./../serviceAccountKey.json");
 
@@ -8,19 +10,21 @@ admin.initializeApp({
   databaseURL: "https://socialape-2db87.firebaseio.com"
 });
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
-
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get("/screams", (req, res) => {
   admin
     .firestore()
     .collection("screams")
+    .orderBy("createdAt", "desc")
     .get()
     .then(data => {
       let screams = [];
       data.forEach(doc => {
-        screams.push(doc.data());
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
       return res.json(screams);
     })
@@ -29,15 +33,11 @@ exports.getScreams = functions.https.onRequest((req, res) => {
     });
 });
 
-exports.addScream = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    return res.status(400).json({ error: "GET Method not allowed" });
-  }
-
+app.post("/scream", (req, res) => {
   let scream = {
     userHandle: req.body.userHandle,
     body: req.body.body,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
   admin
     .firestore()
@@ -51,3 +51,5 @@ exports.addScream = functions.https.onRequest((req, res) => {
       res.status(500).json({ error: "error adding new scream" });
     });
 });
+
+exports.api = functions.https.onRequest(app);
